b6c43c087828de68601a35e8c07b552fb5f1783d
Bharat Chavhan <bharatchavhan141@gmail.com>
2023-02-08 21:35:42 +0530

    random color generator 2.0

--- Random Color Generator/NewProject/index.js
+++ Random Color Generator/NewProject/index.js
@@ -1,12 +1,17 @@
 const main = document.getElementById("main");
 const brtColor = document.getElementById("btnColor");
+const brtColorTwo = document.getElementById("btnColorTwo");
+const colorOne = document.getElementById("colorsOne");
+const colorTwo = document.getElementById("colorsTwo");
+const simple = document.getElementById("simple")
+const gradientColor = document.getElementById("gradient")
 // const colors = document.getElementById("lable");
 
 
 brtColor.addEventListener("click", function() {
     let color = getRandomColor();
-    main.style.backgroundColor = color;
-    brtColor.innerText = color;
+    colorOne.style.backgroundColor = color;
+    simple.innerText = color;
 
   });
   
@@ -19,3 +24,29 @@ brtColor.addEventListener("click", function() {
     }
     return color;
   }
+
+
+// --------------*-------------------------
+
+
+function getRandomColor2() {
+  return `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
+}
+
+function setGradient() {
+  const color1 = getRandomColor2();
+  const color2 = getRandomColor2();
+  
+  colorTwo.style.background = `linear-gradient(to right, ${color1}, ${color2})`;
+  gradientColor.innerText = `linear-gradient(to right, ${color1}, ${color2})`;
+}
+
+
+
+
+
+
+  brtColorTwo.addEventListener('click',function(){
+    setGradient();
+         
+  }) 
